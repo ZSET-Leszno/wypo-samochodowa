69f7985502413a924a2dcaf21dd3482bdc6b1e3e
michustop <michussstop@gmail.com>
2023-03-21 17:58:02 +0100

    fix: mail sender

--- app/Controllers/API/ContactController.php
+++ app/Controllers/API/ContactController.php
@@ -21,6 +21,7 @@ class ContactController extends BaseController
         }
         $mail = new \PHPMailer\PHPMailer\PHPMailer();
         $mail->isSMTP();
+        $mail->CharSet = "UTF-8";
         $mail->Host = $this->config->mail['host'];
         $mail->SMTPAuth = true;
         $mail->Username = $this->config->mail['username'];
@@ -28,12 +29,12 @@ class ContactController extends BaseController
         $mail->SMTPSecure = $this->config->mail['encryption'];
         $mail->Port = $this->config->mail['port'];
         $mail->setFrom($this->config->mail['from']['address'], $this->config->mail['from']['name']);
-        $mail->addAddress($this->config->mail['from']['address'], $this->config->mail['from']['name']);
+        $mail->addAddress($data['email'], $data['name']);
         $mail->isHTML(true);
         $mail->Subject = 'Wiadomość z formularza kontaktowego';
         $mail->Body = '<h1>Wiadomość z formularza kontaktowego</h1><p><b>Imię i nazwisko:</b> ' . $data['name'] . '</p><p><b>Adres e-mail:</b> ' . $data['email'] . '</p><p><b>Temat:</b> ' . $data['subject'] . '</p><p><b>Treść wiadomości:</b> ' . $data['message'] . '</p>';
         if ($mail->send()) {
-            return $this->helper->result(false, 'Wiadomość została wysłana');
+            return $this->helper->result(true, 'Wiadomość została wysłana');
         } else {
             return $this->helper->result(false, 'Wystąpił błąd podczas wysyłania wiadomości');
         }
