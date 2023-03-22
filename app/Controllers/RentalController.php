198ec811616125f3e7c4615cc7d9ce5fd6291e64
michustop <michussstop@gmail.com>
2023-03-22 14:17:39 +0100

    feat: add RentalController.php

---
+++ app/Controllers/RentalController.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Rental controller
+ * @package App\Controllers
+ * @author @michus.studio
+ * @license MIT
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+
+namespace App\Controllers;
+
+class RentalController extends BaseController
+{
+    public function index(int $id)
+    {
+        $car = $this->pdo->query('SELECT * FROM samochody WHERE id = ' . $id);
+        if ($car->rowCount() === 0) {
+            return $this->helper->redirect('home');
+        }
+        $car = $car->fetch();
+        return view('rental', compact('car'));
+    }
+}
