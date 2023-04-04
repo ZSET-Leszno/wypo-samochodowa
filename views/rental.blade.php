485a83c5b158271d3cb1ef6ee8fd1e3bbbb5d45a
michustop <michussstop@gmail.com>
2023-04-05 01:17:41 +0200

    fix: rental view

--- views/rental.blade.php
+++ views/rental.blade.php
@@ -150,7 +150,7 @@
                                     </span>
                                 <div class="info_content">
                                     <span class="meta">VIN</span>
-                                    <span class="title">{{ substr($car['VIN'], 10)}}...</span>
+                                    <span class="title">{{ substr($car['VIN'], 0, -7)}}...</span>
                                 </div>
                             </div>
                         </div>
